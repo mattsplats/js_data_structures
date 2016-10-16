8a66f2323dde5bcee81da2c17ca9ef2beb575db9
Matt Davis <matt_davis@me.com>
2016-10-16 12:40:27 -0500

    Insertion testing (push vs insert at end)

--- singly_linked.js
+++ singly_linked.js
@@ -1,4 +1,4 @@
-function makeNode (arr) {
+function makeList (arr) {
 	let node, next;
 
 	for (let i = arr.length - 1; i >= 0; i--) {
@@ -12,16 +12,14 @@ function makeNode (arr) {
 	return node;
 }
 
-function Node (arr, i, next) {
-	this.data = arr[i];
-	i++;
-	this.next = i < arr.length ? new Node(arr, i) : next;
+function Node (data, next) {
+	this.data = data;
+	this.next = next;
 }
 
 function List (listArr=[]) {
 	this.length = listArr.length;
-	this.firstNode = makeNode(listArr);
-	// this.firstNode = listArr.length > 0 ? new Node(listArr, 0, undefined) : undefined;
+	this.firstNode = makeList(listArr);
 }
 
 List.prototype = {
@@ -62,12 +60,12 @@ List.prototype = {
 		// if (index > this.length || index < 0) throw 'SinglyList: insert(index) out of range';
 
 		if (index === 0) {
-			this.firstNode = new Node([data], 0, this.firstNode);
+			this.firstNode = new Node(data, this.firstNode);
 		} else {
 			let node = this.firstNode;
 			for (let i = 0; i < index - 1; i++) { node = node.next; }
 			
-			node.next = new Node([data], 0, node.next);
+			node.next = new Node(data, node.next);
 		}
 
 		this.length++;
@@ -94,44 +92,50 @@ List.prototype = {
 let startTime;
 let diff = [];
 
-// startTime = process.hrtime();
-
-// let arr = [];
-// for (let i = 0; i < 100; i++) {
-// 	arr.push(i);
-// }
-
-// diff = process.hrtime(startTime);
-// console.log(diff);
+let arr = [];
+let list = new List();
 
 const runs = 10000;
-const arr = [0,1,2,3,4,5,6,7,8,9,10];
 
-let testArr = [];
+startTime = process.hrtime();
+diff = process.hrtime(startTime);
 
+startTime = process.hrtime();
 for (let i = 0; i < runs; i++) {
-	startTime = process.hrtime();
-
-	testArr.push(arr);
-	testArr.push(arr);
-	testArr.push(arr);
-	testArr.push(arr);
-	testArr.push(arr);
-
-	// list1 = new List(arr);
-	// list2 = new List(arr);
-	// list3 = new List(arr);
-	// list4 = new List(arr);
-	// list5 = new List(arr);
-
-	diff[i] = process.hrtime(startTime);
+	list.insert(i);
 }
+diff = process.hrtime(startTime);
+console.log(diff);
 
-// console.log(diff);
-console.log(diff.map(a => a[1]).reduce((a,b) => b > 10000 ? a : a+b) / runs);
+startTime = process.hrtime();
+diff = process.hrtime(startTime);
 
-// for (let i = 0; i < 100; i++) {
-// 	list.insert(i);
-// }
+startTime = process.hrtime();
+for (let i = 0; i < runs; i++) {
+	arr.push(i);
+}
+diff = process.hrtime(startTime);
+console.log(diff);
 
-// 
+arr = [];
+list = new List();
+
+startTime = process.hrtime();
+diff = process.hrtime(startTime);
+
+startTime = process.hrtime();
+for (let i = 0; i < runs; i++) {
+	arr.push(i);
+}
+diff = process.hrtime(startTime);
+console.log(diff);
+
+startTime = process.hrtime();
+diff = process.hrtime(startTime);
+
+startTime = process.hrtime();
+for (let i = 0; i < runs; i++) {
+	list.insert(i);
+}
+diff = process.hrtime(startTime);
+console.log(diff);
